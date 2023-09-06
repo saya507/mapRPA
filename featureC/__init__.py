2914d49dae78177479c87d88396e598924cfb492
saya <a211410n@gmail.com>
2023-09-06 17:51:42 +0900

    メール送信機能を連携できるように関数、mainを書き加えました。

--- featureC/__init__.py
+++ featureC/__init__.py
@@ -1,72 +1,114 @@
-##メール送信機能
+"""
+Send E-Mail with GMail.
 
-from google_auth_oauthlib.flow import InstalledAppFlow
-from email.mime.text import MIMEText
-import base64
+Usage:
+  sendmail.py <sender> <to> <subject> <message_text_file_path>  [--attach_file_path=<file_path>] [--cc=<cc>]
+  sendmail.py -h | --help
+  sendmail.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version. 
+  --attach_file_path=<file_path>     Path of file attached to message.
+  --cc=<cc>     cc email address list(separated by ','). Default None.
+"""
+import pickle
+import os.path
 from googleapiclient.discovery import build
-import smtplib
-from email.utils import formatdate
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import base64
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from pathlib import Path
+
 from email.mime.multipart import MIMEMultipart
 import mimetypes
 from apiclient import errors
-# from gmail_credential import get_credential
+from featureC.gmail_credential import get_credential
 from docopt import docopt
 import logging
-import gmail_credential
 
 logger = logging.getLogger(__name__)
 
 
-##ーメール作成から下書き保存までー##
-#APIのスコープを設定(どのAPIを使うのかの設定)
-SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
-
-##アクセストークンの取得
-def get_credential():
-    launch_browser = True
-    flow = InstalledAppFlow.from_client_secrets_file("../client_id.json", SCOPES)
-    flow.run_local_server()
-    cred = flow.credentials
-    return cred
-
-##下書きの作成
-def create_message(sender, to, subject, message_text):
+def create_message(sender, to, subject, message_text, cc=None):
+    """
+    MIMEText を base64 エンコードする
+    """
     enc = "utf-8"
     message = MIMEText(message_text.encode(enc), _charset=enc)
     message["to"] = to
     message["from"] = sender
     message["subject"] = subject
+    if cc:
+        message["Cc"] = cc
     encode_message = base64.urlsafe_b64encode(message.as_bytes())
     return {"raw": encode_message.decode()}
 
-##下書きフォルダに保存
-def create_draft(service, user_id, message_body):
-    message = {'message':message_body}
-    draft = service.users().drafts().create(userId=user_id, body=message).execute()
-    return draft
 
-##実行
-def main(sender, to, subject, message_text):
-    creds = get_credential()
-    service = build("gmail", "v1", credentials=creds, cache_discovery=False)
-    message = create_message(sender, to, subject, message_text)
-    create_draft(service, "me", message)
+def create_message_with_attachment(
+    sender, to, subject, message_text, file_path, cc=None
+):
+    """
+    添付ファイルつきのMIMEText を base64 エンコードする
+    """
+    message = MIMEMultipart()
+    message["to"] = to
+    message["from"] = sender
+    message["subject"] = subject
+    if cc:
+        message["Cc"] = cc
+    # attach message text
+    enc = "utf-8"
+    msg = MIMEText(message_text.encode(enc), _charset=enc)
+    message.attach(msg)
 
+    content_type, encoding = mimetypes.guess_type(file_path)
 
+    if content_type is None or encoding is not None:
+        content_type = "application/octet-stream"
+    main_type, sub_type = content_type.split("/", 1)
+    if main_type == "text":
+        with open(file_path, "rb") as fp:
+            msg = MIMEText(fp.read(), _subtype=sub_type)
+    elif main_type == "image":
+        with open(file_path, "rb") as fp:
+            msg = MIMEImage(fp.read(), _subtype=sub_type)
+    elif main_type == "audio":
+        with open(file_path, "rb") as fp:
+            msg = MIMEAudio(fp.read(), _subtype=sub_type)
+    else:
+        with open(file_path, "rb") as fp:
+            msg = MIMEBase(main_type, sub_type)
+            msg.set_payload(fp.read())
+    p = Path(file_path)
+    msg.add_header("Content-Disposition", "attachment", filename=p.name)
+    message.attach(msg)
 
-if __name__ == "__main__":
-    sender = "a211410n@gmail.com"
-    to = "a211410n@gmail.com"
-    subject = "件名"
-    message_text = "本文"
-    main(sender=sender, to=to, subject=subject, message_text=message_text)
+    encode_message = base64.urlsafe_b64encode(message.as_bytes())
+    return {"raw": encode_message.decode()}
 
-
-##ー下書き済みのメールを送信するー##
-SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
-SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 def send_message(service, user_id, message):
+    """
+    メールを送信する
+
+    Parameters
+    ----------
+    service : googleapiclient.discovery.Resource
+        Gmail と通信するためのリソース
+    user_id : str
+        利用者のID
+    message : dict
+        "raw" を key, base64 エンコーディングされた MIME Object を value とした dict
+
+    Returns
+    ----------
+    なし
+    """
     try:
         sent_message = (
             service.users().messages().send(userId=user_id, body=message).execute()
@@ -76,6 +118,7 @@ def send_message(service, user_id, message):
     except errors.HttpError as error:
         logger.info("An error occurred: %s" % error)
         raise error
+
 
 #  メイン処理
 def main(sender, to, subject, message_text, attach_file_path, cc=None):
@@ -94,64 +137,86 @@ def main(sender, to, subject, message_text, attach_file_path, cc=None):
     # メール送信
     send_message(service, "me", message)
 
-# プログラム実行部分
-if __name__ == "__main__":
-    arguments = docopt(__doc__, version="0.1")
-    sender = arguments["<sender>"]
-    to = arguments["<to>"]
-    cc = arguments["--cc"]
-    subject = arguments["<subject>"]
-    message_text_file_path = arguments["<message_text_file_path>"]
-    attach_file_path = arguments["--attach_file_path"]
 
-    logging.basicConfig(level=logging.DEBUG)
+def kick_main(flag,cus_address,pdf, sender):
+    # 成功
+    if flag == "success": #変数決めて変更
+        subject = "地図取得成功のお知らせ"
+        message_text_file_path = "suc.text"
+        logging.basicConfig(level=logging.DEBUG)
+        with open(message_text_file_path, "r", encoding="utf-8") as fp:
+            message_text = fp.read()
+        
+        main(
+        sender=sender,
+        to=cus_address,
+        subject=subject,
+        message_text=message_text,
+        attach_file_path=pdf,
+        )
 
-    with open(message_text_file_path, "r", encoding="utf-8") as fp:
-        message_text = fp.read()
+    # 失敗
+    if flag == "false": #変数決めて変更
+        subject = "地図取得失敗のお知らせ"
+        message_text_file_path = "fail_all.text"
 
-    main(
+        logging.basicConfig(level=logging.DEBUG)
+        with open(message_text_file_path, "r", encoding="utf-8") as fp:
+            message_text = fp.read()
+        
+        main(
+        sender=sender,
+        to=cus_address,
+        subject=subject,
+        message_text=message_text,
+        attach_file_path=pdf,
+        )
+
+    # 一部失敗
+    if flag == "half":
+        to = cus_address
+        subject = "地図取得一部失敗のお知らせ"
+        message_text_file_path = "fail.text"
+        attach_file_path = pdf #変更
+
+        logging.basicConfig(level=logging.DEBUG)
+        with open(message_text_file_path, "r", encoding="utf-8") as fp:
+            message_text = fp.read()
+        
+        main(
         sender=sender,
         to=to,
         subject=subject,
         message_text=message_text,
         attach_file_path=attach_file_path,
-        cc=cc,
-    )
+        )
 
 
 
 
 
+# # プログラム実行部分
+# if __name__ == "__main__":
+#     arguments = docopt(__doc__, version="0.1")
+#     sender = arguments["<sender>"]
+#     to = arguments["<to>"]
+#     cc = arguments["--cc"]#"--"あってもなくても良いもの　一旦飛ばして最後に書き足す。（コマンド：sender to subject message...）
+#     subject = arguments["<subject>"]
+#     message_text_file_path = arguments["<message_text_file_path>"]
+#     attach_file_path = arguments["--attach_file_path"]#""の中のコピー、スペース、ファイルのパス
+#     # arguments ：コマンドで入力する
 
+#     logging.basicConfig(level=logging.DEBUG)
 
+#     # withとは、ファイルのopenからcloseについて、インデントが終われば勝手にcloseしてくれる。
+#     with open(message_text_file_path, "r", encoding="utf-8") as fp:
+#         message_text = fp.read()
 
-# ##1 正常に実行された場合、ユーザーに正しくPDFが送信される
-# ##　!!!　テスト用PDFを作ってやってみる。
-# if result == true:
-#     sender = "a211410n@gmail.com"
-#     to = "a211410n@gmail.com" #お客さんのアドレス（変数：cusAdr）
-#     subject = "地図取得成功"
-#     message_text = "地図が取得できました。{PDF}"
-#     main(sender=sender, to=to, subject=subject, message_text=message_text)
-
-# ##2 失敗時にユーザーにメールでエラーが通知される
-# ##4 取得できなかった理由（住所入力が正しくない、対象地域外など）がユーザーに通知される
-# if result == false:
-#     sender = "a211410n@gmail.com"
-#     to = "a211410n@gmail.com" #お客さんのアドレス（変数：cusAdr）
-#     subject = "地図取得失敗"
-#     message_text = "地図ができませんでした。" ##Bで関数に入れたテキストを代入？
-#     main(sender=sender, to=to, subject=subject, message_text=message_text)
-
-# ##3 一部のPDFが取得できなかった場合、そのことがユーザーに通知され、取得されたPDFは送信される
-# ##4 取得できなかった理由（住所入力が正しくない、対象地域外など）がユーザーに通知される
-# if result == little:
-#     sender = "a211410n@gmail.com"
-#     to = "a211410n@gmail.com" #お客さんのアドレス（変数：cusAdr）
-#     subject = "地図取得一部成功"
-#     message_text = "取得できた地図はこちらです。{PDF}取得できなかった地図はこちらです。{原因}" ##Bで関数に入れたテキストを代入？
-#     main(sender=sender, to=to, subject=subject, message_text=message_text)
-
-
-
-##5 ユーザーへの通知を見やすくする（html形式）
+#     main(
+#         sender=sender,
+#         to=to,
+#         subject=subject,
+#         message_text=message_text,
+#         attach_file_path=attach_file_path,
+#         cc=cc,
+#     )
